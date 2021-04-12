74887d1b083ea1f7e2b9c8e8cc80d0a1b8fa32a2
Jacob <jacobliem1@gmail.com>
2021-04-12 13:53:28 -0500

    commit tree 62c45da9ac2544cb83ee129e56c4e530d5d91794

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+ function appendCat(name){
+    const newCat = [...cats, name];
+    return newCat
+}
+
+function prependCat(name){
+    const newCat = [name, ...cats];
+    return newCat
+}
+
+function removeLastCat(){
+    const lastCat = cats.slice(0, 2);
+    return lastCat
+}
+
+function removeFirstCat(){
+    const firstCat = cats.slice(-2);
+    return firstCat
+}
+
+
+
+
+
+
+
+
+
